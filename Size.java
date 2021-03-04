3f07000e4e6d061f6250a63fa3f13f341a93c571
João Santana <170221007@estudantes.ips.pt>
2021-03-04 11:33:46 +0000

    javadoc

--- Size.java
+++ Size.java
@@ -15,19 +15,34 @@ public enum Size {
  this.maxValue=maxValue;
  this.code=code;
  }
+ /**
+  * return String description of size
+  */
  @Override
  public String toString() {
  return this.description;
  }
+ /**
+  * return String description
+  */
  public String getDescription() {
  return description;
  }
+ /**
+  * return int minValue
+  */
  public int getMinValue() {
  return minValue;
  }
+ /**
+  * return int maxValue
+  */
  public int getMaxValue() {
  return maxValue;
  }
+ /**
+  * return int code
+  */
  public char getCode() {
  return code;
  }
